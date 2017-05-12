68831e813681ded23ae5c1f090d6172fc8de20a8
im04 <921198047@qq.com>
2017-05-12 16:12:09 +0800

    提交

---
+++ ser.js
@@ -0,0 +1,45 @@
+var http = require('http'),
+    fs = require('fs'),
+    url  = require("url"),
+    path = require("path");
+
+http.createServer(function(req,res){
+    var pathname=__dirname+url.parse(req.url).pathname;
+    if (path.extname(pathname)=="") {
+        pathname+="/";
+    }
+    if (pathname.charAt(pathname.length-1)=="/"){
+        pathname+="index.html";
+    }
+
+    fs.exists(pathname,function(exists) {
+        if (exists) {
+            switch (path.extname(pathname)) {
+                case ".html":
+                    res.writeHead(200, {"Content-Type": "text/html"});
+                    break;
+                case ".js":
+                    res.writeHead(200, {"Content-Type": "text/javascript"});
+                    break;
+                case ".css":
+                    res.writeHead(200, {"Content-Type": "text/css"});
+                    break;
+                case ".gif":
+                    res.writeHead(200, {"Content-Type": "image/gif"});
+                    break;
+                case ".jpg":
+                    res.writeHead(200, {"Content-Type": "image/jpeg"});
+                    break;
+                case ".png":
+                    res.writeHead(200, {"Content-Type": "image/png"});
+                    break;
+                default:
+                    res.writeHead(200, {"Content-Type": "application/octet-stream"});
+            }
+
+            fs.readFile(pathname, function (err, data) {
+                res.end(data);
+            });
+        }
+    })
+}).listen(3000);
